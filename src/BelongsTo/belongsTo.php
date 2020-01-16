44728f49c5ff6873b0b3e424f646f4ead9cb28ef
Patrick Attema <patrick.attema@studio-eijsink.nl>
2020-01-16 14:36:53 +0100

    Update package

---
+++ src/BelongsTo/belongsTo.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace MrPAttema\withDefault;
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class BelongsTo extends BelongsTo
+{
+    /**
+     * Initialize the relation on a set of models.
+     *
+     * @param  array   $models
+     * @param  string  $relation
+     * @return array
+     */
+    public function initRelation(array $models, $relation)
+    {
+        foreach ($models as $model) {
+            $model->setRelation($relation, $this->getDefaultFor($model));
+        }
+        return $models;
+    }
+
+    /**
+     * Get the results of the relationship.
+     *
+     * @return mixed
+     */
+    public function getResults()
+    {
+        return $this->query->first() ?: $this->getDefaultFor($this->parent);
+    }
+}
